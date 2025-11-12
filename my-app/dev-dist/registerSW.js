642984acadf9576af6f55393542ca17730ffe192
Qrp34ch <168339978+Qrp34ch@users.noreply.github.com>
2025-11-12 19:04:58 +0300

    bench

--- my-app/dev-dist/registerSW.js
+++ my-app/dev-dist/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) navigator.serviceWorker.register('/RIP_frontend/dev-sw.js?dev-sw', { scope: '/RIP_frontend/', type: 'module' })
+if('serviceWorker' in navigator) navigator.serviceWorker.register('/RIP_frontend/dev-sw.js?dev-sw', { scope: '/RIP_frontend/', type: 'classic' })
--- my-app/src/pages/ReactionsPage.tsx
+++ my-app/src/pages/ReactionsPage.tsx
@@ -8,6 +8,7 @@ import { setSearchQuery, resetFilters, clearSearch } from '../store/filterSlice'
 import './ReactionsPage.css';
 import search from "../assets/search.png"
 import { BreadCrumbs } from '../components/BreadCrumbs';
+import synthesis from "../assets/korzina.png"
 
 export const ReactionsPage: FC = () => {
   const dispatch = useAppDispatch();
@@ -157,14 +158,14 @@ export const ReactionsPage: FC = () => {
           <div className="foot">
             {synthesisCount > 0 ? (
               <Link to="/synthesis/1" className="cart-link">
-                <img src="http://localhost:9000/aspirinimages/img/korzina.png" className="cart" alt="cart" />
+                <img src={synthesis} className="cart" alt="cart" />
                 <div className="cart-indicator">
                   <span className="cart-count">{synthesisCount}</span>
                 </div>
               </Link>
             ) : (
               <span className="cart-link disabled">
-                <img src="http://localhost:9000/aspirinimages/img/korzina.png" className="cart" alt="cart" />
+                <img src={synthesis} className="cart" alt="cart" />
               </span>
             )}
           </div>
