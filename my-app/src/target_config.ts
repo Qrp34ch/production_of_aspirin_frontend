d29defb0b6c234f49a89552587d3748cce22b206
Qrp34ch <168339978+Qrp34ch@users.noreply.github.com>
2025-11-27 13:09:49 +0300

    cvb

--- my-app/src/target_config.ts
+++ my-app/src/target_config.ts
@@ -1,13 +1,20 @@
-const target_tauri = true;
+// const target_tauri = true;
 
-export const API_BASE_URL = target_tauri 
-  ? 'http://192.168.0.102:8080/API'  // Для Tauri - прямой IP
-  : '/API';                       // Для веб - proxy
+// export const API_BASE_URL = target_tauri 
+//   ? 'http://192.168.0.102:8080/API'  // Для Tauri - прямой IP
+//   : '/API';                       // Для веб - proxy
 
-export const IMAGE_BASE_URL = target_tauri
-  ? 'http://192.168.0.102:9000'  // Для Tauri - прямой IP
-  : '';                          // Для веб - относительные пути
+// export const IMAGE_BASE_URL = target_tauri
+//   ? 'http://192.168.0.102:9000'  // Для Tauri - прямой IP
+//   : '';                          // Для веб - относительные пути
 
-export const BASE_PATH = target_tauri 
-  ? '' 
-  : '/RIP_frontend';
+// export const BASE_PATH = target_tauri 
+//   ? '' 
+//   : '/RIP_frontend';
+
+// https://24206b2a8fee94.lhr.life
+export const API_BASE_URL = 'https://24206b2a8fee94.lhr.life/API'
+
+export const IMAGE_BASE_URL = 'http://192.168.0.102:9000'
+
+export const BASE_PATH = '/RIP_frontend'
